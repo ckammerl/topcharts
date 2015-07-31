3cfc2c007885765497d158f2b22303769f441ce4
Christiane Kammerl <christiane@kammerl.com>
2015-07-31 16:05:18 -0700

    dev(routing) adds ngResource to communicate with REST API; adds ctrl to child states and deletes parent ctrl

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -2,27 +2,30 @@ angular
     .module('topcharts', [
         'ngAnimate',
         'ui.router',
-        'templates'
+        'templates',
+        'ngResource'
     ])
 
     .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
       $stateProvider
         .state('topcharts', {
           url: '/topcharts',
-          templateUrl: 'topcharts.html',
-          controller: 'TopchartsCtrl'
+          templateUrl: 'topcharts.html'
         })
         .state('topcharts.paid', {
           url: '/paid',
-          templateUrl: 'topcharts/paid.html'
+          templateUrl: 'topcharts/paid.html',
+          controller: 'PaidCtrl'
         })
         .state('topcharts.free', {
           url: '/free',
-          templateUrl: 'topcharts/free.html'
+          templateUrl: 'topcharts/free.html',
+          controller: 'FreeCtrl'
         })
-       .state('topcharts.all', {
+       .state('topcharts.top-grossing', {
           url: '/top-grossing',
-          templateUrl: 'topcharts/all.html'
+          templateUrl: 'topcharts/top_grossing.html',
+          controller: 'TopGrossingCtrl'
         })
 
         // default route
